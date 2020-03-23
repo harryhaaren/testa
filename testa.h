810c7e17981a4cbe5d68bdb76d4cc5b39a3b99f0
Harry van Haaren <harryhaaren@gmail.com>
2020-03-23 17:56:48 +0000

    refactor example steps parsing
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- testa.h
+++ testa.h
@@ -71,11 +71,17 @@ struct testa_context_t {
 
 	/* scenario runner state */
 	uint32_t in_examples;
+
+	/* scenario parsing stores steps and then re-execute them */
+	uint16_t scenario_steps[32];
+	char *scenario_step_strings[32];
+	uint16_t num_scenario_steps;
 };
 
-int32_t testa_ctx_register_steps(struct testa_context_t *ctx,
-				 struct testa_step_t *steps,
-				 uint32_t num_steps)
+int32_t
+testa_ctx_register_steps(struct testa_context_t *ctx,
+			 struct testa_step_t *steps,
+			 uint32_t num_steps)
 {
 	for (uint32_t i = 0; i < num_steps; i++) {
 		if(num_steps >= 32) {
@@ -90,9 +96,22 @@ int32_t testa_ctx_register_steps(struct testa_context_t *ctx,
 	return 0;
 }
 
-int32_t testa_ctx_execute_step(struct testa_context_t *ctx,
-			       const char *string,
-			       void *userdata)
+int32_t
+testa_ctx_example_parsing(struct testa_context_t *ctx,
+			  const char *string)
+{
+	(void)ctx;
+
+	/* Figure out how to cleanly split rows of data into
+	 * executable iterations of each test
+	 */
+	printf("example str: %s", string);
+	return 0;
+}
+
+int32_t
+testa_ctx_steps_parse(struct testa_context_t *ctx,
+		      const char *string)
 {
 	/* find correct string */
 	uint32_t found = 0;
@@ -114,14 +133,17 @@ int32_t testa_ctx_execute_step(struct testa_context_t *ctx,
 		return 0;
 	}
 
-	/* TODO: implement examples! */
-	if (ctx->in_examples)
-		return 0;
-
 	/* empty lines */
 	int len = strlen(string);
-	if (len < 2)
+	if (len < 2) {
+		ctx->in_examples = 1;
 		return 0;
+	}
+
+	if (ctx->in_examples) {
+		testa_ctx_example_parsing(ctx, string);
+		return 0;
+	}
 
 	for (uint32_t i = 0; i < ctx->num_steps; i++) {
 		/* TODO: implement fuzzy search here? */
@@ -142,42 +164,61 @@ int32_t testa_ctx_execute_step(struct testa_context_t *ctx,
 	default: return -2; /* error in doubled finding */
 	}
 
+	/* Append this step to the runtime list */
+	ctx->scenario_steps[ctx->num_scenario_steps] = found_idx;
+	ctx->scenario_step_strings[ctx->num_scenario_steps] = strdup(string);
+	ctx->num_scenario_steps++;
+	return 0;
+}
 
-	struct testa_step_t *step = ctx->steps[found_idx];
+int32_t
+testa_ctx_steps_execute(struct testa_context_t *ctx,
+			void *userdata)
+{
+	for(uint16_t i = 0; i < ctx->num_scenario_steps; i++) {
+		uint16_t step_idx = ctx->scenario_steps[i];
+		struct testa_step_t *step = ctx->steps[step_idx];
+		char *string = ctx->scenario_step_strings[i];
 
-	/* No variables handles early and return */
-	if (step->cb_none_t) {
-		ctx->steps[found_idx]->cb_none_t(string, userdata);
-		return 0;
+		/* No variables handles early and return */
+		if (step->cb_none_t) {
+			step->cb_none_t(string, userdata);
+			continue;
+		}
+
+		/* find first < in string to get variable value */
+		char *first_gt_symbol = strstr(string, "<");
+		if (!first_gt_symbol) {
+			printf("variable callback without <variable> in step.\n"
+			       "Ensure step in .features file has a <variable>\n");
+			return -1;
+		}
+
+		int first_gt_len = strlen(first_gt_symbol);
+		if (first_gt_len < 1) {
+			return -5;
+		}
+		// move past < symbol to number
+		first_gt_symbol = &first_gt_symbol[1];
+
+		int parse_base = 0;
+		if (step->cb_uint32_t) {
+			uint32_t value = strtoul(first_gt_symbol, NULL, parse_base);
+			int err = step->cb_uint32_t(string, value, userdata);
+			if(err)
+				return -5;
+			continue;
+		}
+		if (step->cb_int32_t) {
+			int32_t value = strtol(first_gt_symbol, NULL, parse_base);
+			int err = step->cb_int32_t(string, value, userdata);
+			if(err)
+				return -5;
+			continue;
+		}
 	}
 
-	/* find first < in string to get variable value */
-	char *first_gt_symbol = strstr(string, "<");
-	if (!first_gt_symbol) {
-		printf("variable callback without <variable> in step.\n"
-		       "Ensure step in .features file has a <variable>\n");
-		return -1;
-	}
-
-	int first_gt_len = strlen(first_gt_symbol);
-	if (first_gt_len < 1) {
-		return -5;
-	}
-	// move past < symbol to number
-	first_gt_symbol = &first_gt_symbol[1];
-
-	int parse_base = 0;
-	if (step->cb_uint32_t) {
-		uint32_t value = strtoul(first_gt_symbol, NULL, parse_base);
-		return step->cb_uint32_t(string, value, userdata);
-	}
-	if (step->cb_int32_t) {
-		int32_t value = strtol(first_gt_symbol, NULL, parse_base);
-		return step->cb_int32_t(string, value, userdata);
-	}
-
-	printf("no valid handler for %s found, fail\n", string);
-	return -3;
+	return 0;
 }
 
 int32_t
@@ -198,10 +239,11 @@ testa_scenario_run_from_file(struct testa_context_t *ctx,
 	ssize_t read;
 
 	while ((read = getline(&line, &len, file)) != -1) {
-		int32_t err = testa_ctx_execute_step(ctx, line, userdata);
+		int32_t err = testa_ctx_steps_parse(ctx, line);
 		if (err) {
 			if(err == -1) { /* not found */
-				printf("No handler found for this step, add callback code to handle it!\n  %s",
+				printf("No handler found for this step"
+				       "add callback code to handle it!\n  %s",
 				       line);
 			} else {
 				printf("line %s returns err: %d\n", line, err);
@@ -209,6 +251,13 @@ testa_scenario_run_from_file(struct testa_context_t *ctx,
 		}
 		step_counter++;
 	}
+
+	printf("steps parsed, %d steps\n", step_counter);
+
+	/* TODO: now execute the steps with the parsed examples */
+	int32_t err = testa_ctx_steps_execute(ctx, userdata);
+	if(err)
+		printf("error executing steps\n");
 
 	free(line);
 	fclose(file);
